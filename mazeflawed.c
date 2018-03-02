1e7ebd5b57b181c688294115e0243a27b31dd5cd
pciesl2 <pcieslik@protonmail.com>
2018-03-01 19:40:22 -0600

    Stack struct & maze pos

--- mazeflawed.c
+++ mazeflawed.c
@@ -1,19 +1,65 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define TRUE 1
+#define FALSE 0
 
-typedef struct mazeStruct
-{
+typedef struct mazeStruct{
  // char arr[32][32];  /* allows for a maze of size 30x30 plus outer walls */
  char **arr;           /* allows for a dynamic 2-D maze of any size */
+ int **arr2;          /* dynamic 2-D array to keep track of visted positions */
  int xsize, ysize;
  int xstart, ystart;
  int xend, yend;
 } maze;
 
-int main (int argc, char **argv)
+struct nodeStruct
 {
-  maze m1;
+ int xpos, ypos;
+ struct nodeStruct*  next;
+};
+ 
+typedef struct nodeStruct node;
+typedef node* nodePtr; 
 
+struct listStruct
+{
+  nodePtr   head;
+};
+
+typedef struct listStruct list;
+/* call a function to intialize the list encased in a structure */
+list init ()
+{
+  list s;
+  s.head = NULL;
+  return s;
+}
+
+void insertAtFront (node** hd, int x, int y)
+{
+ node* ptr = (node*) malloc (sizeof(node));
+ ptr->xpos = x;
+ ptr->ypos = y;
+ ptr->next = *hd;       
+ *hd = ptr;      
+}
+
+void removeFromFront (node** hd)
+{
+ node* ptr = *hd;
+
+ if (ptr != NULL)
+   {
+    *hd = ptr->next;
+    free (ptr);
+   }
+}
+
+int main (int argc, char **argv){
+  maze m1, mpos;
+  node* head = NULL;
+  list s1 = init();
+  
   int xpos, ypos;
   int i,j;
 
@@ -43,11 +89,15 @@ int main (int argc, char **argv)
   printf ("start: %d, %d\n", m1.xstart, m1.ystart);
   printf ("end: %d, %d\n", m1.xend, m1.yend);
 
-  /* allocate the maze */
+  /* allocate the maze and position array */
   m1.arr = (char **) malloc (sizeof(char *) * (m1.xsize+2) );
   for (i = 0; i < m1.xsize+2; i++)
      m1.arr[i] = (char *) malloc (sizeof(char ) * (m1.ysize+2) );
-
+  
+  m1.arr2 = (int **) malloc (sizeof(int *) * (m1.xsize+2) );
+  for (i = 0; i < m1.xsize+2; i++)
+     m1.arr2[i] = (int *) malloc (sizeof(int ) * (m1.ysize+2) );
+  
   /* initialize the maze to empty */
   for (i = 0; i < m1.xsize+2; i++)
      for (j = 0; j < m1.ysize+2; j++)
@@ -74,6 +124,15 @@ int main (int argc, char **argv)
     {
      m1.arr[xpos][ypos] = '*';
     }
+  /* mark blocked positions/borders as visted */
+  for (i = 0; i < m1.xsize+2; i++)
+    for (j = 0; j < m1.ysize+2; j++)
+    {
+      if(m1.arr[i][j] == '.')
+        m1.arr2[i][j] = FALSE;
+      else if(m1.arr[i][j] == '*')
+        m1.arr2[i][j] = TRUE;
+    }
 
   /* print out the initial maze */
   for (i = 0; i < m1.xsize+2; i++)
@@ -82,4 +141,5 @@ int main (int argc, char **argv)
        printf ("%c", m1.arr[i][j]);
      printf("\n");
     }
+  
 }
